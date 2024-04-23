9eabfa6fb247d24328b3b1c6aa9f41f182d598f8
Manuel Ajanel <aja21791@uvg.edu.gt>
2024-04-23 12:48:35 -0600

    deleting projects section

--- Proyectos/Proyecto1/api/src/db/connection.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize('railway', 'root','khxTMGJmYrANwqDmXtdxWCSGhvpEOYai', {
-    host: 'roundhouse.proxy.rlwy.net',
-    dialect: 'mysql',
-})
-
-export default sequelize;
--- Proyectos/Proyecto1/api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Server from './models/server';
-import dotenv from 'dotenv';
-
-
-//configuracion de variables de ambiente
-dotenv.config();
-
-const server = new Server();
